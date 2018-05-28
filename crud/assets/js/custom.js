355277701c665bb67e6acd4be2d7abfc8c03415a
mkerney <mohan900080460l@gmail.com>
2018-05-28 16:11:12 +0530

    Updated Crud with edit functionality as well

--- crud/assets/js/custom.js
+++ crud/assets/js/custom.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+    var xyz = Math.random().toString(36).substr(2, 9);
     $('.submit-action').on('click', '#formSubmit', function(){
         // console.log(e);
 
@@ -14,7 +16,8 @@ $(document).ready(function(){
 
     function tbodyPart(abc){
         // console.log(abc);
-        var tbodySection = "<tr><td>";
+        console.log(xyz);
+        var tbodySection = "<tr id=\"temp_"+xyz+"\"><td>";
         for(let i = 0; i < abc.length; i++){
             tbodySection+= abc[i] + "</td><td>";    
         }
@@ -37,14 +40,18 @@ $(document).ready(function(){
     }
 
     $('.crudTable').on('click', '.editRow', function(){
+        $('body .submit-action').addClass('edit');
+        var editID = $(this).closest('tr').attr('id');
+        // console.log(editId);
+        $('#crudForm').addClass(editID);
+
         var editStuff = $(this).closest('tr').find('td');
         // console.log(editStuff);
         var editGrabedDetails = [];
         $(editStuff).each(function() {
             // debugger;
-            // console.log($(this).text());
+            console.log($(this).text());
             editGrabedDetails.push($(this).html());
-
         });
         console.log(editGrabedDetails);
         $('input#formName').val(editGrabedDetails[0]);
@@ -54,6 +61,46 @@ $(document).ready(function(){
         $('input#formHeight').val(editGrabedDetails[4]);
         $('input#formWeight').val(editGrabedDetails[5]);
         $('input#formESign').val(editGrabedDetails[6]);
+
+
+        $('body').on('click', '#saveSubmit', function(){
+            $('body .submit-action').removeClass('edit');
+            
+            var saveForm = $('#crudForm').attr('class');
+            console.log(saveForm);
+            if(saveForm == editID){
+                var inputDetails = [];
+                // var name = $('#formName').val();
+                // inputDetails.push(name);
+                // console.log(inputDetails);
+                $( "input" ).each(function( index ) {
+                    inputDetails.push( $(this).val() );
+                });
+                // console.log(inputDetails);
+                var replaceStuff = $('#'+saveForm+' > td');
+                $(replaceStuff[0]).text(inputDetails[0]);
+                $(replaceStuff[1]).text(inputDetails[1]);
+                $(replaceStuff[2]).text(inputDetails[2]);
+                $(replaceStuff[3]).text(inputDetails[3]);
+                $(replaceStuff[4]).text(inputDetails[4]);
+                $(replaceStuff[5]).text(inputDetails[5]);
+                $(replaceStuff[6]).text(inputDetails[6]);
+
+                // var i = 0;
+                // i < $(replaceStuff).length-1;
+                // console.log($(replaceStuff).length-1);
+                // $(replaceStuff).each(function(){
+                //     console.log($(replaceStuff).text());
+                //     $(replaceStuff).text(inputDetails[i]);
+                //     i++;
+                // });
+                $( "input" ).each(function( ) {
+                    $(this).val("");
+                });
+            }
+    
+        });
+    
     });
     $('.crudTable').on('click', '.deleteRow', function(){
         //deleteDetails();
