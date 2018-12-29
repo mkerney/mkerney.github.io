1e286c5a4c2d22467929a0cdfccc98c22c235bc0
mkerney <mohan900080460l@gmail.com>
2018-12-29 23:04:40 +0530

    Updated UI for the GHPages with mkerney.github.io/

---
+++ build/precache-manifest.5bd0020de25b87be08e47f08f01205fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a4e542e83b82c981a8a7e0c3706676b",
+    "url": "/static/media/event-hyderabad.6a4e542e.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "142d5256724106b8768c",
+    "url": "/static/js/main.142d5256.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "142d5256724106b8768c",
+    "url": "/static/css/main.e5bce0ae.chunk.css"
+  },
+  {
+    "revision": "1da55f4658a5005053c362fc9a43b9b5",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.2be9c231d51f04a24465891a57df06f6.js"
+  "/precache-manifest.5bd0020de25b87be08e47f08f01205fe.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/build/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
