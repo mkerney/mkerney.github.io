1fb1a63c974641f22c19d53b0352de274cbecc79
mohan-wal <mohan.k@westagilelabs.com>
2020-04-26 20:19:30 +0530

    Added build for the code

---
+++ build/precache-manifest.aa261cc2b77d7bd27c2962310c1d5f3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "04855a2712c4c3f5c560",
+    "url": "/static/css/main.bbca91aa.chunk.css"
+  },
+  {
+    "revision": "04855a2712c4c3f5c560",
+    "url": "/static/js/main.04855a27.chunk.js"
+  },
+  {
+    "revision": "7679921ca0e482a23396",
+    "url": "/static/js/1.7679921c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d689282ae512075ec108e05296d8178f",
+    "url": "/static/media/mineNew.d689282a.jpeg"
+  },
+  {
+    "revision": "23dc17e57e7d4575f3a4bb9b7bf6d490",
+    "url": "/static/media/charminar-hyderabad.23dc17e5.jpg"
+  },
+  {
+    "revision": "8d9ea844d17c72c474874b9d24c30e55",
+    "url": "/index.html"
+  }
+];
